13931a780513114d598b36b48ee594565d658799
Ranee Bofa Jan <user9@ip-172-31-18-53.ap-south-1.compute.internal>
2026-01-21 09:53:40 +0000

    add jenkinsfile and updated main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,25 @@ provider "aws" {
  
 module "Vms_app1" {
   #source = "./module/ec2-instance"
-  source = "git::https://github.com/raneeyclai/tf-aws-module-jan26.git//module/ec2-instance?ref=main"
+  source = "git::https://github.com/raneeyclai/tf-aws-module-jan26.git//ec2-instance?ref=main"
   region-1 = "ap-south-1"
   instance_type = "t3.micro"
   subnets = ["subnet-0a413d769fe98ba0b", "subnet-043f1f8151b681cca", "subnet-04f47a3252bdf06e6"]
   num_of_vm = 1
   env = "dev"
+  web_sg = [module.Web_SG01.web_sg_id]
 }
  
 output "vm_private_ips01" {
   value = module.Vms_app1.vm_priv_pips
+}
+
+module "Web_SG01" {
+  source = "git::https://github.com/raneeyclai/tf-aws-module-jan26.git//security_groups?ref=main"
+  project = "Ranee"
+  vpc_id = data.aws_vpc.default.id
+}
+ 
+data "aws_vpc" "default" {
+  default = true
 }
